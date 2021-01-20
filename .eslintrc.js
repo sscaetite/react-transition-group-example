d28cc0d04b336c62a11eb11c04ca2237f837b94c
Samuel Caetité <samuel.caetite@vorttex.com.br>
2021-01-20 18:39:50 -0300

    Initial config

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+/* eslint-disable no-undef */
+module.exports = {
+	'env': {
+		'browser': true,
+		'es2021': true
+	},
+	'extends': [
+		'eslint:recommended',
+		'plugin:react/recommended'
+	],
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 12,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react'
+	],
+	'rules': {
+		'indent': [
+			'error',
+			'tab'
+		],
+		'linebreak-style': [
+			'error',
+			'unix'
+		],
+		'quotes': [
+			'error',
+			'single'
+		],
+		'semi': [
+			'error',
+			'never'
+		],
+		'max-len': [
+			'error',
+			{
+				'code': 80,
+				'ignoreUrls': true
+			}
+		]
+	}
+}
